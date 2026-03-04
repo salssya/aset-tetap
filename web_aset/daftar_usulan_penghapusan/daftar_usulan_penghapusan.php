c17018c8bb91d55162214d468737a020b55e561f
Salsabila Tasya A <181396982+salssya@users.noreply.github.com>
2026-03-04 14:42:56 +0700

    perubahan pada pageLength

--- web_aset/daftar_usulan_penghapusan/daftar_usulan_penghapusan.php
+++ web_aset/daftar_usulan_penghapusan/daftar_usulan_penghapusan.php
@@ -1077,7 +1077,7 @@ $stmt_docs->close();
   document.addEventListener('DOMContentLoaded', function () {
   if ($('#usulanTable tbody tr td[colspan]').length === 0) {
     $('#usulanTable').DataTable({
-      ordering: true, searching: true, paging: true, pageLength: 25,
+      ordering: true, searching: true, paging: true, pageLength: 10,
       language: { url: '../../dist/js/i18n/id.json' },
       scrollX: true,
       columnDefs: [{ orderable: false, targets: [1, 2, 10, 11] }]
@@ -1086,7 +1086,7 @@ $stmt_docs->close();
 
   if ($('#dokumenTable tbody tr td[colspan]').length === 0) {
     $('#dokumenTable').DataTable({
-      ordering: true, searching: true, paging: true, pageLength: 25,
+      ordering: true, searching: true, paging: true, pageLength: 10,
       language: { url: '../../dist/js/i18n/id.json' },
       scrollX: true,
       columnDefs: [{ orderable: false, targets: [5] }]
